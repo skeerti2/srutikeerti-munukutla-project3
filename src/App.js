31aa51bd68a5b76e0af6a58b1fff2ed7d425aa85
Sruti Keerti <munukutla.s@northeastern.edu>
2021-12-03 14:27:48 -0800

    push latest changes to branch

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './components/Homepage.jsx';
+
+function App() {
+  return (
+    <div className="container">
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
